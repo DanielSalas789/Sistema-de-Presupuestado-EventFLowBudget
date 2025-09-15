230d4046d34ab8f5b1502194d46488fcb7386ea0
Daniel Salas <salascruzdanieleduardo7@gmail.com>
2025-09-15 01:46:56 -0600

    base

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>EventFlow Budget</h1>
+      <ul>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/crear">Crear Presupuesto</Link></li>
+        <li><Link to="/presupuestos">Ver Presupuestos</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../styles/pages.css";
+
+function Home() {
+  return (
+    <div className="page">
+      <h2>Bienvenido a EventFlow Budget 🎉</h2>
+      <p>Administra fácilmente presupuestos para eventos y salones.</p>
+    </div>
+  );
+}
+
+export default Home;
