4e5579a3f45026142fb141da6cde889bb690fdb2
Daniel Salas <salascruzdanieleduardo7@gmail.com>
2025-09-15 01:09:45 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Sistema-de-Presupuestado-EventFLowBudget/", // Usa el nombre exacto de tu repo
   plugins: [react()],
-  base: "/Sistema-de-Presupuestado-EventFLowBudget/",
 });
