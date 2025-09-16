2dbcd7c89c85f9ec4f293241a95044c1cabbfc05
Daniel Salas <salascruzdanieleduardo7@gmail.com>
2025-09-16 00:50:38 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/crear" element={<CrearPresupuesto />} />
           <Route path="/presupuestos" element={<VerPresupuestos />} />
-          <Route path="*" element={<div>Página no encontrada</div>} />
         </Routes>
       </div>
     </div>
