e5d5614968e902d7557e708a0584abbece021ecb
Daniel Eduardo Salas <salascruzdanieleduardo7@gmail.com>
2025-10-03 18:42:15 -0600

    conexion bds arreglar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 // App.jsx - Solo UN Router aquí
-import React from "react";
+import React, { use, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
 import CrearPresupuesto from "./pages/CrearPresupuesto";
-import Dashboard from "../pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import Presupuestos from "./Pages/VerPresupuestos";
+import Home from "./Pages/Home";
 import "./Styles/App.css";
 
 function App() {
+  // Estado para almacenar los productos obtenidos del backend
+  const [productos, setProductos] = useState([]);
+  const fetchProductos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/productos");
+      if (!response.ok) {
+        throw new Error("Error en la respuesta de la red");
+      }
+      const data = await response.json();
+      setProductos(data);
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/productos")
+      .then((response) => response.json())
+      .then((data) => setProductos(data))
+      .catch((error) =>
+        console.error("Error al obtener los productos:", error)
+      );
+  }, []);
+
   return (
     // Configuración principal del enrutador de la aplicación
     // SOLO existe un Router en toda la app (mejor práctica)
@@ -22,14 +47,12 @@ function App() {
           {/* Sistema de rutas de la aplicación */}
           <Routes>
             {/* Ruta para la página de inicio (dashboard) */}
-            <Route path="/" element={<Dashboard />} />
-
+            <Route path="/" element={<Home productos={productos} />} />{" "}
+            {/* 👈 Paso productos al Home */}
             {/* Ruta alternativa para el dashboard */}
             <Route Component={Dashboard} path="/dashboard" />
-
             {/* Ruta para la creación de nuevos presupuestos */}
             <Route path="/crear-presupuesto" element={<CrearPresupuesto />} />
-
             {/* Ruta para visualizar el listado de presupuestos */}
             <Route path="/presupuestos" element={<Presupuestos />} />
           </Routes>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
-
-const Home = () => {
+const Home = ({ productos }) => {
   return (
     <div className="page">
       <h1>Bienvenido a EventFlow Budget</h1>
       <p>Gestiona tus presupuestos de eventos fácilmente.</p>
+
+      <h2>Lista de Productos</h2>
+      <ul>
+        {productos.map((p) => (
+          <li key={p.id_producto}>
+            {p.nombre} - ${p.precio}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default Home;
