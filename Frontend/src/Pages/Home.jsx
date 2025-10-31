a2ce7678119f4be85581e8cebac4c4de50776e93
Daniel Eduardo Salas <salascruzdanieleduardo7@gmail.com>
2025-10-31 13:06:30 -0600

    Home

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,6 +5,8 @@ function Home({ productos }) {
     <div className="home-page">
       <h2>🏠 Bienvenido al Sistema de Cotización de Eventos</h2>
       <p>Consulta los productos disponibles para tu evento:</p>
+
+      <ul></ul>
     </div>
   );
 }
